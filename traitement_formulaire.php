16871067fd1193dcfd81a3fd816db37f592e26b7
Rosinade <60359883+Rosinade@users.noreply.github.com>
2020-06-16 16:21:39 +0200

    form

---
+++ traitement_formulaire.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rosanora | UX/UI Designer</title>
+    <link rel="icon" href="ressources/favicon.png"/>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@800&family=Raleway:ital,wght@0,300;0,600;1,300&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+<?php
+/*
+	********************************************************************************************
+	CONFIGURATION
+	********************************************************************************************
+*/
+// destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par une virgule
+$destinataire = 'rosanora.lam@gmail.com';
+ 
+// copie ? (envoie une copie au visiteur)
+$copie = 'oui';
+ 
+// Action du formulaire (si votre page a des paramètres dans l'URL)
+// si cette page est index.php?page=contact alors mettez index.php?page=contact
+// sinon, laissez vide
+$form_action = '';
+ 
+// Messages de confirmation du mail
+$message_envoye = "Votre message nous est bien parvenu !";
+$message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
+ 
+// Message d'erreur du formulaire
+$message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
+ 
+/*
+	********************************************************************************************
+	FIN DE LA CONFIGURATION
+	********************************************************************************************
+*/
+ 
+/*
+ * cette fonction sert à nettoyer et enregistrer un texte
+ */
+function Rec($text)
+{
+	$text = htmlspecialchars(trim($text), ENT_QUOTES);
+	if (1 === get_magic_quotes_gpc())
+	{
+		$text = stripslashes($text);
+	}
+ 
+	$text = nl2br($text);
+	return $text;
+};
+ 
+/*
+ * Cette fonction sert à vérifier la syntaxe d'un email
+ */
+function IsEmail($email)
+{
+	$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
+	return (($value === 0) || ($value === false)) ? false : true;
+}
+ 
+// formulaire envoyé, on récupère tous les champs.
+$nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
+$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
+$objet   = (isset($_POST['objet']))   ? Rec($_POST['objet'])   : '';
+$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
+ 
+// On va vérifier les variables et l'email ...
+$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
+$err_formulaire = false; // sert pour remplir le formulaire en cas d'erreur si besoin
+ 
+if (isset($_POST['envoi']))
+{
+	if (($nom != '') && ($email != '') && ($objet != '') && ($message != ''))
+	{
+		// les 4 variables sont remplies, on génère puis envoie le mail
+		$headers  = 'From:'.$nom.' <'.$email.'>' . "\r\n";
+		//$headers .= 'Reply-To: '.$email. "\r\n" ;
+		//$headers .= 'X-Mailer:PHP/'.phpversion();
+ 
+		// envoyer une copie au visiteur ?
+		if ($copie == 'oui')
+		{
+			$cible = $destinataire.';'.$email;
+		}
+		else
+		{
+			$cible = $destinataire;
+		};
+ 
+		// Remplacement de certains caractères spéciaux
+		$caracteres_speciaux     = array('&#039;', '&#8217;', '&quot;', '<br>', '<br />', '&lt;', '&gt;', '&amp;', '…',   '&rsquo;', '&lsquo;');
+		$caracteres_remplacement = array("'",      "'",        '"',      '',    '',       '<',    '>',    '&',     '...', '>>',      '<<'     );
+ 
+		$objet = html_entity_decode($objet);
+		$objet = str_replace($caracteres_speciaux, $caracteres_remplacement, $objet);
+ 
+		$message = html_entity_decode($message);
+		$message = str_replace($caracteres_speciaux, $caracteres_remplacement, $message);
+ 
+		// Envoi du mail
+		$num_emails = 0;
+		$tmp = explode(';', $cible);
+		foreach($tmp as $email_destinataire)
+		{
+			if (mail($email_destinataire, $objet, $message, $headers))
+				$num_emails++;
+		}
+ 
+		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
+		{
+			echo '<p>'.$message_envoye.'</p>';
+		}
+		else
+		{
+			echo '<p>'.$message_non_envoye.'</p>';
+		};
+	}
+	else
+	{
+		// une des 3 variables (ou plus) est vide ...
+		echo '<p>'.$message_formulaire_invalide.'</p>';
+		$err_formulaire = true;
+	};
+}; // fin du if (!isset($_POST['envoi']))
+ 
+if (($err_formulaire) || (!isset($_POST['envoi'])))
+{
+	// afficher le formulaire
+	echo '
+	<form id="contact" method="post" action="'.$form_action.'">
+	<fieldset><legend>Vos coordonnées</legend>
+		<p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" value="'.stripslashes($nom).'" /></p>
+		<p><label for="email">Email :</label><input type="text" id="email" name="email" value="'.stripslashes($email).'" /></p>
+	</fieldset>
+ 
+	<fieldset><legend>Votre message :</legend>
+		<p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" value="'.stripslashes($objet).'" /></p>
+		<p><label for="message">Message :</label><textarea id="message" name="message" cols="30" rows="8">'.stripslashes($message).'</textarea></p>
+	</fieldset>
+ 
+	<div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer le formulaire !" /></div>
+	</form>';
+};
+
+?>
+
+</body>
